adac9d5656e39979bc0942cbd82192abf36fb031
Kevin Hwang <k3vinhwang@gmail.com>
2015-02-16 18:07:13 -0800

    Move the camera up so the floor is visible

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ var initThree = function () {
 
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
   camera.position.z = 5;
+  camera.position.y = 5;
   scene.add(camera);
 
   // Floor
