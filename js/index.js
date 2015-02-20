58ed43c511fbbee313140684961e9545bfe6be7d
Kevin Hwang <k3vinhwang@gmail.com>
2015-02-19 20:18:39 -0800

    Clicking shoots

--- js/index.js
+++ js/index.js
@@ -11,6 +11,14 @@ var timeStep = 1/60;
 var boxes = [];
 var boxMeshes = [];
 
+// Projectiles
+var balls = [];
+var ballMeshes = [];
+var ballShape, ballGeometry;
+var shootDirection, shootVelo;
+var projector;
+
+
 // Pointer Lock
 var controls, time = Date.now();
 
@@ -126,6 +134,11 @@ var updatePhysics = function () {
     boxMeshes[i].position.copy(boxes[i].position);
     boxMeshes[i].quaternion.copy(boxes[i].quaternion);
   }
+
+  for (i = 0; i < balls.length; i++ ){
+    ballMeshes[i].position.copy(balls[i].position);
+    ballMeshes[i].quaternion.copy(balls[i].quaternion);
+  }
 };
 
 var pointerLock = function () {
@@ -172,7 +185,65 @@ var onWindowResize = function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 };
 
+// Shooting
+
+var initShooting = function () {
+  window.addEventListener("click", shoot);
+
+  ballShape = new CANNON.Sphere(0.2);
+  ballGeometry = new THREE.SphereGeometry(ballShape.radius, 32, 32);
+  shootDirection = new THREE.Vector3();
+  shootVelo = 15;
+  projector = new THREE.Projector();
+};
+
+
+var shoot = function () {
+  if (!controls.enabled) {
+    return;
+  }
+
+  var x = sphereBody.position.x;
+  var y = sphereBody.position.y;
+  var z = sphereBody.position.z;
+
+  var ballBody = new CANNON.Body({ mass: 1 });
+  ballBody.addShape(ballShape);
+
+  var ballMesh = new THREE.Mesh(ballGeometry, material);
+  ballMesh.castShadow = true;
+  ballMesh.receiveShadow = true;
+
+  world.add(ballBody);
+  scene.add(ballMesh);
+
+  balls.push(ballBody);
+  ballMeshes.push(ballMesh);
+
+  getShootDir(shootDirection);
+
+  ballBody.velocity.set(shootDirection.x * shootVelo, shootDirection.y * shootVelo, shootDirection.z * shootVelo);
+
+  // Move the ball outside the player sphere
+  x += shootDirection.x * (sphereShape.radius*1.02 + ballShape.radius);
+  y += shootDirection.y * (sphereShape.radius*1.02 + ballShape.radius);
+  z += shootDirection.z * (sphereShape.radius*1.02 + ballShape.radius);
+  ballBody.position.set(x,y,z);
+  ballMesh.position.set(x,y,z);
+};
+
+var getShootDir = function (targetVec) {
+  var vector = targetVec;
+  targetVec.set(0,0,1);
+  projector.unprojectVector(vector, camera);
+  var ray = new THREE.Ray(sphereBody.position, vector.sub(sphereBody.position).normalize());
+  targetVec.x = ray.direction.x;
+  targetVec.y = ray.direction.y;
+  targetVec.z = ray.direction.z;
+};
+
 pointerLock();
 initCannon();
 initThree();
+initShooting();
 animate();
